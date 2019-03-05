af09ca6f724ae7eba53edb11284fe41c01e2c5fd
DaveFres <nanyandave@gmail.com>
2019-03-05 16:11:06 +0300

    Finishing Touches: Improving the UX

--- app.js
+++ app.js
@@ -184,7 +184,11 @@ var UIController = (function() {
     return (type === 'exp' ?  '-':  '+') + ' ' + int + '.' + dec;
   };
 
-
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
 
   return {
     getInput: function() {
@@ -273,12 +277,6 @@ var UIController = (function() {
       
       var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(current, index) {
         if (percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -304,6 +302,22 @@ var UIController = (function() {
 
     },    
 
+    changedType: function() {
+
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType + ',' +
+        DOMstrings.inputDescription + ',' +
+        DOMstrings.inputValue
+      );
+      
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
@@ -324,6 +338,8 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
   };
 
