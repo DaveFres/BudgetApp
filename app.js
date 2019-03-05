eafe7567758cf1bfa7ab8df10218a7ccd79c7859
DaveFres <nanyandave@gmail.com>
2019-03-05 15:09:00 +0300

    Added displaying the Current Month and Year

--- app.js
+++ app.js
@@ -153,7 +153,8 @@ var UIController = (function() {
     expensesLabel: '.budget__expenses--value',
     percentageLabel: '.budget__expenses--percentage',
     container: '.container',
-    expensesPercLabel: '.item__percentage'
+    expensesPercLabel: '.item__percentage',
+    dateLabel: '.budget__title--month',
   };
 
   var formatNumber = function(num, type) {
@@ -288,7 +289,20 @@ var UIController = (function() {
 
     },
 
-    
+    displayMonth: function() {
+
+      var now, months, month, year;
+
+      var now = new Date();
+
+      months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 
+      'October', 'November', 'December'];
+      month = now.getMonth();
+
+      year = now.getFullYear();
+      document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' +  year;
+
+    },    
 
     getDOMstrings: function() {
       return DOMstrings;
@@ -395,6 +409,7 @@ var controller = (function(budgetCtrl, UICtrl) {
   return {
     init: function() {
       console.log("app has started");
+      UICtrl.displayMonth();
       UICtrl.displayBudget({
         budget: 0,
         totalInc: 0,
